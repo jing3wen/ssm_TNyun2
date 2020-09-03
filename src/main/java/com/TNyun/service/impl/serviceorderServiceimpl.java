60e207f0392b89bf51422874e4a24cb44214513b
沈靖文 <1584591284@qq.com>
2020-09-03 11:38:25 +0800

    9月3号靖文第二次提交：删除了一些多余文档

--- src/main/java/com/TNyun/service/impl/serviceorderServiceimpl.java
+++ src/main/java/com/TNyun/service/impl/serviceorderServiceimpl.java
@@ -1,48 +1,51 @@
 package com.TNyun.service.impl;
-
-import com.TNyun.dao.serviceorderMapper;
+import com.TNyun.entity.customer;
 import com.TNyun.entity.serviceorder;
 import com.TNyun.service.serviceorderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Calendar;
 
-/**
- * @Author jingwen
- * @Date 2020/8/9 15:55
- * (ﾉꐦ ๑´Д`๑)ﾉ彡┻━┻  I have to type the code again   (▼皿▼#)
- **/
+
 @Service
-public class serviceorderServiceimpl implements serviceorderService {
+public class serviceorderServiceImpl implements serviceorderService{
 
     @Autowired
-    private serviceorderMapper serviceordermapper;
+    private com.TNyun.dao.serviceorderMapper serviceorderMapper;
 
+    //顾客购买子系统服务
     @Override
-    public String addService(serviceorder ser) {
-        if(ser!=null){
-            System.out.println("要添加的服务: "+ser.ser_print());
-            serviceordermapper.addservice(ser);
-            return "add service is ok";
-        }
-        else return "add service is error";
+    public String addService(customer cus,int S_id,String S_name){
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String rent_time = formatter.format(calendar.getTime());
 
+        serviceorder so=new serviceorder();
+
+        so.setRent_time(rent_time);
+        so.setTime(rent_time);
+        so.setC_id(cus.getId());
+        so.setC_name(cus.getName());
+        so.setS_id(S_id);
+        so.setS_name(S_name);
+        so.setAgree("0");
+
+        serviceorderMapper.addservice(so);
+        return "Apply to become an enterprise user is ok";
     }
 
+    //顾客删除子系统服务
     @Override
-    public String deleteService(int c_id, int s_id) {
-        if(c_id!=0&&s_id!=0){
-            System.out.println("要删除的service的 c_id="+Integer.toString(c_id)+", s_id="+Integer.toString(s_id));
-            serviceordermapper.deleteOneByC_idAndS_id(c_id,s_id);
-            return "delete service is ok";
-        }
-       else return "delete service is error";
+    public String deleteService(int c_id,int s_id){
+        System.out.println("用户"+c_id+"要删除的服务是:"+s_id);
+        serviceorderMapper.deleteOneByC_idAndS_id(c_id,s_id);
+        return "删除成功";
     }
-
+    //查询相关顾客所有购买的子系统服务
     @Override
-    public List<serviceorder> findallByC_id(int c_id) {
-        System.out.println("要查找 c_id="+Integer.toString(c_id)+"的所有已购买服务");
-        return serviceordermapper.findallByC_id(c_id);
+    public List<serviceorder> findallByC_id(int c_id){
+        return serviceorderMapper.findallByC_id(c_id);
     }
 }
