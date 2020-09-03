c732d20e412f7657d39982a903a29b711b126b18
沈靖文 <1584591284@qq.com>
2020-09-03 11:13:28 +0800

    9月3号，靖文提交：完成用户注册的邮箱验证，添加一个工具包utils,发送邮箱的类SendMail,添加激活成功页面

--- src/main/java/com/TNyun/controller/customerController.java
+++ src/main/java/com/TNyun/controller/customerController.java
@@ -6,11 +6,9 @@ import com.TNyun.utils.SendMail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
-import java.util.Random;
 
 /**
  * @Author jingwen
@@ -50,7 +48,7 @@ public class customerController {
     public String activateCustomerByPhone(String phone){
         System.out.println("收到activateCustomer: ");
         System.out.println(customerService.activateCustomerByPhone(phone));
-        return "activateCustomer is ok";
+        return "customer/activateSuccess";
     }
 
 
