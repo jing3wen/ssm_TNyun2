f36099527fca32bae8beadf5f6b876d56e833923
征 薛 <15829309186@163.com>
2020-09-05 21:03:59 +0800

    SI_adminController的补充

--- src/main/java/com/TNyun/controller/SI_adminController.java
+++ src/main/java/com/TNyun/controller/SI_adminController.java
@@ -24,21 +24,35 @@ public class SI_adminController {
     private SI_adminService SI_adminService;
 
     @RequestMapping("/purchaselist")
-    public ModelAndView list(Model model, Page page){
-        PageHelper.offsetPage(page.getStart(),page.getCount());
-        List<serviceorder> purchaselist=serviceorderService.findall();
-        int total=(int) new PageInfo<>(purchaselist).getTotal();
+    public ModelAndView list(Model model, Page page) {
+        PageHelper.offsetPage(page.getStart(), page.getCount());
+        List<serviceorder> purchaselist = serviceorderService.findall();
+        int total = (int) new PageInfo<>(purchaselist).getTotal();
         page.setTotal(total);
 
-        model.addAttribute("purchaselist",purchaselist);
-        ModelAndView modelAndView=new ModelAndView("admin/purchaselist");
-        return modelAndView;
-    }
-    @RequestMapping("/setagree1")
-    public ModelAndView del_ccustomer(int c_id,int s_id){
-        SI_adminService.AgreeExperience(c_id,s_id);
-        ModelAndView modelAndView=new ModelAndView("redirect:purchaselist")   ;
+        model.addAttribute("purchaselist", purchaselist);
+        ModelAndView modelAndView = new ModelAndView("admin/purchaselist");
         return modelAndView;
     }
 
+    @RequestMapping("/setagree1")
+    public ModelAndView setagree1(int c_id, int s_id) {
+        SI_adminService.AgreeExperience(c_id, s_id);
+        ModelAndView modelAndView = new ModelAndView("redirect:purchaselist");
+        return modelAndView;
+    }
+
+    @RequestMapping("/setagree2")
+    public ModelAndView setagree2(int c_id, int s_id) {
+        SI_adminService.AgreePurchase(c_id, s_id);
+        ModelAndView modelAndView = new ModelAndView("redirect:purchaselist");
+        return modelAndView;
+    }
+
+    @RequestMapping("/setagree3")
+    public ModelAndView setagree3(int c_id, int s_id) {
+        SI_adminService.Overdue(c_id, s_id);
+        ModelAndView modelAndView = new ModelAndView("redirect:purchaselist");
+        return modelAndView;
+    }
 }
